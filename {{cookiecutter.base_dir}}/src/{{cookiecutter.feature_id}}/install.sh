0ddca7d0716647a7021a8b70e49df11e8bc41c59
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-12 19:38:37 +0000

    fix: bash shimi first line

--- {{cookiecutter.base_dir}}/src/{{cookiecutter.feature_id}}/install.sh
+++ {{cookiecutter.base_dir}}/src/{{cookiecutter.feature_id}}/install.sh
@@ -1,9 +1,8 @@
-{% set optional_aptget_packages = (cookiecutter.content.aptget | selectattr('optional', 'equalto', 'True') | list) %}
-{% set mandatory_aptget_packages = (cookiecutter.content.aptget | selectattr('optional', 'equalto', 'False') | list) %}
+{%- set optional_aptget_packages = (cookiecutter.content.aptget | selectattr('optional', 'equalto', 'True') | list) -%}
+{%- set mandatory_aptget_packages = (cookiecutter.content.aptget | selectattr('optional', 'equalto', 'False') | list) -%}
 #!/usr/bin/env bash
-
+# Code 
 set -e
-
 {% if optional_aptget_packages |length > 0  %} 
 # optional aptget packages  
 {% for aptget_package in optional_aptget_packages -%} 
@@ -40,12 +39,10 @@ INCLUDE{{ pipx_package.display_name| to_env_case }}="true"
 # Clean up
 rm -rf /var/lib/apt/lists/*
 
-
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
-
 
 # Checks if packages are installed and installs them if not
 check_packages() {
@@ -73,13 +70,20 @@ fi
 check_packages ${aptget_packages[@]}
 {% endif %}
 
-
 {% if cookiecutter.content.pipx is defined and cookiecutter.content.pipx |length > 0  %} 
+# code bellow is mostly taken from the base python feature https://raw.githubusercontent.com/devcontainers/features/main/src/python/install.sh
+updaterc() {
+    echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+    if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+        echo -e "$1" >> /etc/bash.bashrc
+    fi
+    if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+        echo -e "$1" >> /etc/zsh/zshrc
+    fi
+}
 # settings these will allow us to clean leftovers later on
 export PYTHONUSERBASE=/tmp/pip-tmp
 export PIP_CACHE_DIR=/tmp/pip-tmp/cache
-
-
 # install python if does not exists
 if ! type pip3 > /dev/null 2>&1; then
     echo "Installing python3..."
@@ -92,16 +96,35 @@ if ! type pip3 > /dev/null 2>&1; then
     export INSTALLTOOLS="false"
     curl -fsSL https://raw.githubusercontent.com/devcontainers/features/main/src/python/install.sh | $SHELL
 fi
-
-# configuring install location 
-# changing /usr/local/bin as we know it will be in PATH of all users
-export PIPX_BIN_DIR="/usr/local/bin"
+# install pipx if not exists
+export PIPX_HOME=${PIPX_HOME:-"/usr/local/py-utils"}
+export PIPX_BIN_DIR="${PIPX_HOME}/bin"
 
 if ! type pipx > /dev/null 2>&1; then
-    echo "Installing pipx..."
-    export PIPX_HOME=/opt/pipx
-    mkdir -p ${PIPX_HOME}
-    chmod -R g+r+w "${PIPX_HOME}"
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+
+    PATH="${PATH}:${PIPX_BIN_DIR}"
+
+    # Create pipx group, dir, and set sticky bit
+    if ! cat /etc/group | grep -e "^pipx:" > /dev/null 2>&1; then
+        groupadd -r pipx
+    fi
+    usermod -a -G pipx ${USERNAME}
+    umask 0002
+    mkdir -p ${PIPX_BIN_DIR}
+    chown -R "${USERNAME}:pipx" ${PIPX_HOME}
+    chmod -R g+r+w "${PIPX_HOME}" 
+    find "${PIPX_HOME}" -type d -print0 | xargs -0 -n 1 chmod g+s
 
     pip3 install --disable-pip-version-check --no-cache-dir --user pipx 2>&1
     /tmp/pip-tmp/bin/pipx install --pip-args=--no-cache-dir pipx
@@ -131,6 +154,10 @@ if [ "$INCLUDE{{ pipx_package.display_name | to_env_case }}" = "true" ]; then
 {% endfor %}
 fi
 {% endfor %}
+
+updaterc "export PIPX_HOME=\"${PIPX_HOME}\""
+updaterc "export PIPX_BIN_DIR=\"${PIPX_BIN_DIR}\""
+updaterc "if [[ \"\${PATH}\" != *\"\${PIPX_BIN_DIR}\"* ]]; then export PATH=\"\${PATH}:\${PIPX_BIN_DIR}\"; fi"
 
 # cleaning after pip
 rm -rf /tmp/pip-tmp
